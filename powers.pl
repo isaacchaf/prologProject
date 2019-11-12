807ba24acccba86dc4d8c481f51a2b8e39f2fb4f
Isaac Chavez <46462818+isaacchaf@users.noreply.github.com>
2019-11-11 18:29:29 -0600

    Update powers.pl

--- powers.pl
+++ powers.pl
@@ -28,8 +28,8 @@ first_powers(N,[(Power,Factor)|PFs],[Power|Powers]) :-
           Powers = []
       ;
           N1 is N - 1,
-          remove_power(Power,PFs,PFs1),  % We remove this pair, compute the next power of F (i.e. P*F)
-          Power1 is Power * Factor,  % and insert the pair (P*F,F) into the pair-list, respecting the invariants.
+          remove_power(Power,PFs,PFs1),    % We remove this pair, compute the next power of F (i.e. P*F)
+          Power1 is Power * Factor,        % and insert the pair (P*F,F) into the pair-list, respecting the invariants
           sorted_insert(PFs1,(Power1,Factor),PFs2),
           first_powers(N1,PFs2,Powers)
         ).
